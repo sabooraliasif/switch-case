4fdc8c9c6e0931ab6189e3b56b1b83833f82a870
SABOOR ALI ASIF <98776073+sabooraliasif@users.noreply.github.com>
2022-02-09 08:10:39 -0800

    Add files via upload

---
+++ syntax switch c.cpp
@@ -0,0 +1,16 @@
+switch( expression )
+{
+	case value-1:
+			Block-1;
+			Break;
+	case value-2:
+			Block-2;
+			Break;
+	case value-n:
+			Block-n;
+			Break;
+	default:
+			Block-1;
+			Break;
+}
+Statement-x;
